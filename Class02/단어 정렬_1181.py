8655cbe3a4d32a2db98f1f40724a4823d1b81a62
Ik Hyeon Cho <andantej99@ajou.ac.kr>
2023-11-23 16:28:52 +0900

    Class02 1문제 풀이 완료 - 1181 : 단어 정렬

--- Class02/단어 정렬_1181.py
+++ Class02/단어 정렬_1181.py
@@ -1,2 +1,15 @@
 import sys
-sys.stdin = open(r"C:\Users\andan\OneDrive\Desktop\PythonCoTe\CodingTest_Python\Class02\input.txt", "rt")
+sys.stdin = open(r"C:\Users\user\Desktop\CodingTest_Python\Class02\input.txt", "rt")
+
+n = int(input())
+
+set = set()
+for _ in range(n):
+    set.add(input())
+
+lst = list(set)
+lst.sort()
+lst.sort(key=len)
+
+for word in lst:
+    print(word)
