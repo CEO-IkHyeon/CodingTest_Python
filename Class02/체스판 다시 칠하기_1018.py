6b84ef0d26e608de3efc2fc706ffb352f5f49ed8
IkHyeon <andantej281@gmail.com>
2023-11-30 11:48:25 +0900

    Class02 1문제 푸는중 - 1018 : 체스판 다시 칠하기

--- Class02/체스판 다시 칠하기_1018.py
+++ Class02/체스판 다시 칠하기_1018.py
@@ -1,4 +1,23 @@
 import sys
-sys.stdin = open(r"C:\Users\user\Desktop\CodingTest_Python\Class02\input.txt", "rt")
+sys.stdin = open(r"C:\Users\andan\OneDrive\Desktop\PythonCoTe\CodingTest_Python\Class02\input.txt", "rt")
 input = sys.stdin.readline
 
+n, m = map(int, input().split())
+
+grid = []
+checked = [[0 for _ in range(m)] for _ in range(n)]
+
+# w로 시작 -> 짝수행,열은 W || 홀수행, 열은  B
+# B로 시작 _
+for i in range(n):
+    grid.append(list(input().rstrip()))
+  
+print(grid)
+
+ 
+
+#grid 돌면서 8x8 확인하기?
+
+for i in range(n):
+    for j in range(m):
+        
